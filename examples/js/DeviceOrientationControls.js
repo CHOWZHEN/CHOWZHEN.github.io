c7900cf1bf43f351178206e36a2a790bb71c86dd
CHOWZHEN <42401703+CHOWZHEN@users.noreply.github.com>
2019-05-29 00:29:29 +0800

    Update DeviceOrientationControls.js

--- examples/js/DeviceOrientationControls.js
+++ examples/js/DeviceOrientationControls.js
@@ -49,9 +49,9 @@ THREE.DeviceOrientationControls = function ( object ) {
 
 			euler.set( beta, alpha, - gamma, 'YXZ' ); // 'ZXY' for the device, but 'YXZ' for us
 			
-			quaternion=getBaseQuaternion( beta, alpha, - gamma);
+			//quaternion=getBaseQuaternion( beta, alpha, - gamma);
 			
-			//quaternion.setFromEuler( euler ); // orient the device从欧拉角得到四元数
+			quaternion.setFromEuler( euler ); // orient the device从欧拉角得到四元数
 			//quaternion=quaternionMultiply( quaternion,q1); 
             //quaternion=quaternionMultiply( quaternion, q0.setFromAxisAngle( zxuan, - orient )); 
 			quaternion.multiply( q1 ); // camera looks out the back of the device, not the top
